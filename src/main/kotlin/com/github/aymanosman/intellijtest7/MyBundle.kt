902a0cf2236b3417c636dc274abf44b845dd68d4
GitHub Action <action@github.com>
2023-12-26 20:38:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/aymanosman/intellijtest7/MyBundle.kt
+++ src/main/kotlin/com/github/aymanosman/intellijtest7/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.aymanosman.intellijtest7
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/aymanosman/intellijtest7/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/aymanosman/intellijtest7/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.aymanosman.intellijtest7.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
